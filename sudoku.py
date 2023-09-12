2cc86a5313d6f22877a47b07c8e2def33dafccd9
Savoyevatel <71535298+Savoyevatel@users.noreply.github.com>
2023-09-12 10:28:04 +0200

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,54 +1,8 @@
+import numpy as np
+
 z = 9 #your signature
-c= 0 #counting zeros
 
 grid = [[0 for i in range(z)] for j in range(z)]
-
-#print(a)
-
-pair = [[0, 0] for i in range(c)]
-
-def dis(a):
-    print("The solution is!")
-    for i in range(z):
-        for j in range(z):
-            print(a[i][j], end="   ")
-        print("\n")
-        print()
-
-#print(dis())
-def check(a, l):
-    zero = 1
-    for i in range(z):
-        for j in range(z):
-            if a[i][j] == 0:
-                l[0] = i #row
-                l[1] = j #column
-                return True
-    return False
-#print(check(i,j))
-def correct(grid, row, col, num):
-    # Check if 'num' is not in the current row, column, or 3x3 box
-    for i in range(9):
-        if (grid[row][i] == num) or (grid[i][col] == num) or (grid[row - row % 3 + i // 3][col - col % 3 + i % 3] == num):
-            return False
-    return True
-
-def sudoku(grid):
-    l = [0, 0]
-    if (not check(grid, l)):
-        return True
-    row = l[0]
-    col = l[1]
-
-    for num in range(1, 10):
-        if correct(grid, row, col, num):
-            grid[row][col] = num
-            if (sudoku(grid)):
-                return True
-
-            grid[row][col] = 0
-    return False
-
 with open("sudoku.txt", "r") as f:
     c = 0
     for i in range(z):
@@ -58,7 +12,91 @@ with open("sudoku.txt", "r") as f:
             if grid[i][j] == 0:
                 c += 1
 
-if (sudoku(grid)):
-    dis(grid)
-else:
+
+def dis(a):
+    print("The solution is!")
+    for i in range(z):
+        for j in range(z):
+            print(a[i][j], end="   ")
+        print("\n")
+        print()
+
+
+grid_2 = np.transpose(np.asarray(grid)).tolist()
+
+
+def check_duplicates(lst):
+    for sub_list in lst:
+        seen = set()
+        for item in sub_list:
+            if item != 0:
+                if item in seen:
+                    return True
+                seen.add(item)
+    return False
+
+
+grid3 = []
+for i in range(0, len(grid), 3):
+    temp_lists = [[], [], []]
+    for j in range(i, i + 3):
+        sublist = grid[j]
+
+        for k in range(3):
+            start_index = k * 3
+            end_index = start_index + 3
+            temp_lists[k] += sublist[start_index:end_index]
+
+    grid3.extend(temp_lists)
+
+
+def dis(a):
+    print("The solution is!")
+    for i in range(z):
+        for j in range(z):
+            print(a[i][j], end="   ")
+        print("\n")
+        print()
+
+
+def check(a, l):
+    zero = 1
+    for i in range(z):
+        for j in range(z):
+            if a[i][j] == 0:
+                l[0] = i
+                l[1] = j
+                return True
+    return False
+
+
+def correct(grids, rows, col, num):
+    # Check if 'num' is not in the current row, column, or 3x3 box
+    for i in range(9):
+        if (grids[rows][i] == num) or (grids[i][col] == num) or (grids[rows - rows % 3 + i // 3][col - col % 3 + i % 3]
+                                                                 == num):
+            return False
+    return True
+
+
+def sudoku(grids):
+    pairs = [0, 0]
+    if not check(grids, pairs):
+        return True
+    rows = pairs[0]
+    cols = pairs[1]
+
+    for num in range(1, 10):
+        if correct(grid, rows, cols, num):
+            grid[rows][cols] = num
+            if sudoku(grid):
+                return True
+
+            grid[rows][cols] = 0
+    return False
+
+
+if check_duplicates(grid) or check_duplicates(grid_2) or check_duplicates(grid3):
     print("The puzzle cannot be solved!!\nThe Question is wrong!")
+elif sudoku(grid):
+    dis(grid)
