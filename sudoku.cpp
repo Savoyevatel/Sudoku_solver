b856951b83e7ea0e9a9c5476dd9b6e80526b26ec
Savoyevatel <71535298+Savoyevatel@users.noreply.github.com>
2023-09-13 18:39:36 +0200

    added constrains if the initial sudoku.txt is not properly formulated.

--- sudoku.cpp
+++ sudoku.cpp
@@ -16,6 +16,23 @@ bool dis(std::vector<std::vector<int>> & a) {
     }
 }
 
+
+bool checkduplicates(const std::vector<std::vector<int>>& lst) {
+    for (const auto& subList : lst) {
+        std::set<int> seen;
+        for (int item : subList) {
+            if (item != 0) {
+                if (seen.count(item) > 0) {
+                    return true;
+                }
+                seen.insert(item);
+            }
+        }
+    }
+    return false;
+}
+
+
 bool check(std::vector<std::vector<int>>& a, std::vector<int>& pair) {
     for (int row = 0; row < z; row++) {
         for (int col = 0; col < z; col++) {
@@ -80,11 +97,45 @@ int main()
         }
         file.close();
     }
+    
+    
+    std::vector<std::vector<int>> grid2;
+    
+    for (int j = 0; j < 9; j++) {
+        std::vector<int> row;
+        for (int i = 0; i < 9; i++) {
+            row.push_back(grid[j][i]);
+        }
+        grid2.push_back(row);
+    }
 
-    if (sudoku(grid)) {
+    
+    std::vector<std::vector<int>> grid3;
+
+    for (int i = 0; i < grid.size(); i += 3) {
+        std::vector<std::vector<int>> temp_lists = {{}, {}, {}};
+
+        for (int j = i; j < i + 3; j++) {
+            std::vector<int> sublist = grid[j];
+
+            for (int k = 0; k < 3; k++) {
+                int start_index = k * 3;
+                int end_index = start_index + 3;
+                temp_lists[k].insert(temp_lists[k].end(), sublist.begin() + start_index, sublist.begin() + end_index);
+            }
+        }
+
+        grid3.insert(grid3.end(), temp_lists.begin(), temp_lists.end());
+    }
+
+
+    if (checkduplicates(grid) || checkduplicates(grid2) || checkduplicates(grid3)) {
+        std::cout << "The puzzle cannot be solved!!\nThe Question is wrong!" << std::endl;
+    }
+	if (sudoku(grid)) {
         dis(grid);
     } else {
-        std::cout << " The puzzle cannot be solved!!\nThe Question is wrong! - " << std::endl;
+        std::cout << " The puzzle cannot be solved!!\nThe Question is wrong!" << std::endl;
     }
 
     return 0;
